61a5bdb0fb0084a80e49a24e29a3112390a32b32
Elene Sturua <sturuael@grinnell.edu>
2024-10-24 17:20:29 +0200

    chessBoardSpawner

--- Assets/Scripts/chessBoard.cs
+++ Assets/Scripts/chessBoard.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class chessBoard : MonoBehaviour
 {
+    public GameObject whiteBoardTile;
+    public GameObject blackBoardTile;
+    int gridSize =8;
+    float tileCubicSize = 1.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        CreateBoard();
         
     }
 
@@ -12,5 +18,26 @@ public class chessBoard : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CreateBoard()
+    {
+        for (int i=1; i<=gridSize; i++)
+        {
+            for(int j=1; j<=gridSize; j++)
+            {
+                GameObject tile;
+                if ((i+j)% 2 == 0)
+                {
+                    tile = Instantiate(whiteBoardTile, new Vector3(i * tileCubicSize, 0, j * tileCubicSize), Quaternion.identity);
+                }
+                else
+                {
+                    tile = Instantiate(blackBoardTile, new Vector3(i * tileCubicSize, 0, j * tileCubicSize), Quaternion.identity);
+                }
+
+                tile.transform.parent = transform;
+            }
+        }
     }
 }
