7e337136601a8144bd4bf112ea44cb53592c9f21
Liza Platonov <eplatonov@middlebury.edu>
2024-10-26 14:36:57 +0200

    more on rook adn small chessboard

--- Assets/Scripts/Chess Piece Movement/Specific Pieces/RookMovement.cs
+++ Assets/Scripts/Chess Piece Movement/Specific Pieces/RookMovement.cs
@@ -3,23 +3,46 @@ using System.Collections;
 
 public class RookMovement : MonoBehaviour, ChessPieceMovement
 {
+    [SerializeField] float speed = 0.2f;
     private Vector3 position;
-    [SerializeField] float speed;
-    [SerializeField] Transform movePoint;
+
     void Start()
     {
         position = transform.position;
-        
     }   
 
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Move();
+        }
     }
 
     public void Move()
     {
+        Vector3 target = transform.position;
+        target.z -= 2;
+        target.x -= 2;
 
+        //StartCoroutine("MoveRoutine");
+        //.position = transform.position;
+        while (transform.position != target) {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+
+        
+    }
+
+    public IEnumerator MoveRoutine()
+    {
+        Vector3 target = transform.position;
+        target.z -= 2;
+        target.x -= 2;
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        yield return null;
     }
 
     public float[][] CheckAvailableTiles()
