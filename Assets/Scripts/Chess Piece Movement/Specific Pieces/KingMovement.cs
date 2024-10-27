c98e2beb77b11ffa0ec8d89b3fcafdc02d5c07d9
Wing Kiu Lau <wlau@bowdoin.edu>
2024-10-27 17:45:48 +0100

    king movement

--- Assets/Scripts/Chess Piece Movement/Specific Pieces/KingMovement.cs
+++ Assets/Scripts/Chess Piece Movement/Specific Pieces/KingMovement.cs
@@ -1,53 +1,87 @@
 using UnityEngine;
 using System.Collections;
 
-public class KingMovement : MonoBehaviour, ChessPieceMovement
+public class KingMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 1;
-    private Vector3 position;
-    private float tileSize = 2f;
-
-    void Start()
-    {
-        position = transform.position;
-    }
+    [SerializeField] private float speed = 1f;
+    private const float tileSize = 2f;  // Each tile is 2 units apart
+    private const float minX = -9f;
+    private const float maxX = -1f;
+    private const float minZ = -9f;
+    private const float maxZ = -1f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.UpArrow))  
         {
-            Move();
+            Move(Vector3.back);  
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))  
+        {
+            Move(Vector3.forward);  
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) // Move Left
+        {
+            Move(Vector3.right);  
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) // Move Right
+        {
+            Move(Vector3.left);  
+        }
+        else if (Input.GetKeyDown(KeyCode.Q)) // Move Diagonal Up-Left
+        {
+            Move(new Vector3(1, 0, -1)); 
+        }
+        else if (Input.GetKeyDown(KeyCode.E)) // Move Diagonal Up-Right
+        {
+            Move(new Vector3(-1, 0, -1)); 
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) // Move Diagonal Down-Left
+        {
+            Move(new Vector3(1, 0, 1));  
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) // Move Diagonal Down-Right
+        {
+            Move(new Vector3(-1, 0, 1));  
         }
     }
 
-    public void Move()
+    private Vector3 GetTargetPos(Vector3 direction)
     {
-        Vector3 target = GetTargetPos();
+        float targetX = transform.position.x + direction.x * tileSize;
+        float targetZ = transform.position.z + direction.z * tileSize;
 
-        if (target != Vector3.zero) // Ensure target is valid
+        return new Vector3(targetX, transform.position.y, targetZ);
+    }
+
+    public void Move(Vector3 direction)
+    {
+        Vector3 target = GetTargetPos(direction);
+
+        if (IsValidPosition(target) && target != transform.position)
         {
             StartCoroutine(MoveToTarget(target));
         }
     }
 
-    private Vector3 GetTargetPos()
-    {
-        Vector3 target = transform.position + new Vector3(-tileSize, 0, -tileSize); // Example diagonal move
-        return target;
-    }
-
     private IEnumerator MoveToTarget(Vector3 target)
     {
-        while (transform.position != target)
+        while (Vector3.Distance(transform.position, target) > 0.01f)
         {
             transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-            yield return null;
+            yield return null;  
         }
+
+        transform.position = target;
+    }
+
+    private bool IsValidPosition(Vector3 target)
+    {
+        return target.x >= -9 && target.x <= -1 && target.z >= -9 && target.z <= -1;
     }
 
     public float[][] CheckAvailableTiles()
     {
-        return new float[3][];
+        return new float[0][];
     }
-
 }
