5e54527395b93a46635dd4be21e70b13dbe6c4d2
Elene Sturua <sturuael@grinnell.edu>
2024-10-28 13:30:05 +0100

    changed the interface and added the knight movement script

--- Assets/Scripts/Chess Piece Movement/ChessPieceMovement.cs
+++ Assets/Scripts/Chess Piece Movement/ChessPieceMovement.cs
@@ -1,7 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public interface ChessPieceMovement
 {
-    void Move();
-    float[][] CheckAvailableTiles();
+    void Move(Vector3 targetPosition);
+    List<Vector3> CheckAvailableMoves();
 }
--- Assets/Scripts/Chess Piece Movement/Specific Pieces/KnightMovement.cs
+++ Assets/Scripts/Chess Piece Movement/Specific Pieces/KnightMovement.cs
@@ -1,11 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class KnightMovement : MonoBehaviour, ChessPieceMovement
 {
+
+    [SerializeField] float speed = 1;
+    private Vector3 currentPosition;
+    private int boardSize = 8;
+
+
+     private Vector3[] knightMoves = new Vector3[] {new Vector3(2, 0, 1), new Vector3(2, 0, -1),new Vector3(-2, 0, 1), new Vector3(-2, 0, -1),
+        new Vector3(1, 0, 2), new Vector3(-1, 0, 2), new Vector3(1, 0, -2), new Vector3(-1, 0, -2)
+    };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -14,13 +26,57 @@ public class KnightMovement : MonoBehaviour, ChessPieceMovement
         
     }
 
-    public void Move()
-    {
 
+    private bool IsValidMove(Vector3 targetPosition)
+    {
+        return CheckAvailableMoves().Contains(targetPosition);
     }
 
-    public float[][] CheckAvailableTiles()
+
+    private bool IsOnBoard(Vector3 targetPosition)
     {
-        return new float[3][];
+        return targetPosition.x >= 1 && targetPosition.x <= boardSize && targetPosition.z >= 1 && targetPosition.z <= boardSize;
+    }
+
+
+    private IEnumerator MoveTowardsTarget(Vector3 targetPosition)
+    {
+        while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            yield return null;
+        }
+        currentPosition = targetPosition;
+    }
+
+
+
+
+
+    public void Move(Vector3 targetPosition)
+    {
+        if (IsValidMove(targetPosition))
+        {
+            StartCoroutine(MoveTowardsTarget(targetPosition));
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public List<Vector3> CheckAvailableMoves()
+    {
+        List<Vector3> availableMoves = new List<Vector3>();
+
+        foreach (Vector3 move in knightMoves)
+        {
+            Vector3 availablePosition = currentPosition + move;
+            if (IsOnBoard(availablePosition))
+            {
+                availableMoves.Add(availablePosition);
+            }
+        }
+        return availableMoves;
     }
 }
