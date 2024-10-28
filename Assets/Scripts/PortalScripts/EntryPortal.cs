9238efc87d33d098a3c7414aac013029ba31fb6d
Wing Kiu Lau <wlau@bowdoin.edu>
2024-10-28 21:59:03 +0100

    changes to portal

--- Assets/Scripts/PortalScripts/EntryPortal.cs
+++ Assets/Scripts/PortalScripts/EntryPortal.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class PortalEntry : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] ChessBoard chessPiece;
+    private Vector3 exitPosition;
+    private SpriteRenderer portalSprite;
+    private Collider portalCollider;
+
     void Start()
     {
-        
+        portalSprite = GetComponent<SpriteRenderer>();
+        portalCollider = GetComponent<Collider>();
+        portalSprite.enabled = true;
+
     }
 
     // Update is called once per frame
+    // Come into contact with piece
     void Update()
     {
-        
+        chessPiece.enabled = false; 
     }
+
+   
+
+    void Update()
+    {
+        if (level == 5 && frog.starCounter == 2)
+        {
+            portalSR.enabled = true;
+            portalCollider.enabled = true;
+        }
+    }
+
+
 }
--- Assets/Scripts/PortalScripts/ExitPortal.cs
+++ Assets/Scripts/PortalScripts/ExitPortal.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Por : MonoBehaviour
 {
+    private Vector3 exitPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
