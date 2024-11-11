efd291647a3a6d774538696314f6628dbdf695ec
Liza Platonov <eplatonov@middlebury.edu>
2024-11-11 17:40:15 +0100

    clicker fixed

--- Assets/Scripts/TileScripts/Tile.cs
+++ Assets/Scripts/TileScripts/Tile.cs
@@ -8,6 +8,7 @@ public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IP
     private Color originalColor;
     public Color hoverColor = Color.green;
     private Color endGoalColor = Color.HSVToRGB(60, 100,50);
+
     [HideInInspector]
     public ChessBoard chessBoard;
 
@@ -39,7 +40,7 @@ public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IP
     public void SetEndGoal()
     {
        
-            tileRenderer.material.color = endGoalColor;
+        tileRenderer.material.color = endGoalColor;
         isEndGoal = true;
     }
 
