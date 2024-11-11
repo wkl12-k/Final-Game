e1715fcc28cd91e86f94eb51dca6ddd25078bc2e
Elene Sturua <sturuael@grinnell.edu>
2024-11-11 16:57:01 +0100

    hi

--- Assets/Scripts/UIScripts/TutorialScript.cs
+++ Assets/Scripts/UIScripts/TutorialScript.cs
@@ -1,56 +1,56 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class TutorialScript : MonoBehaviour
-{
-    [SerializeField] private GameObject UIElement;
-    [SerializeField] private KeyCode trigger;
-    [SerializeField] private GameObject[] tutorials;
+//public class TutorialScript : MonoBehaviour
+//{
+//    [SerializeField] private GameObject UIElement;
+//    [SerializeField] private KeyCode trigger;
+//    [SerializeField] private GameObject[] tutorials;
 
-    private static bool _hasInitialized = false;
-    private int currentIndex = 0;
+//    private static bool _hasInitialized = false;
+//    private int currentIndex = 0;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(trigger))
-        {
-            if (!_hasInitialized)
-            {
-                UIElement.SetActive(false);
-                _hasInitialized = true;
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(trigger))
+//        {
+//            if (!_hasInitialized)
+//            {
+//                UIElement.SetActive(false);
+//                _hasInitialized = true;
 
-                if (tutorials.Length > 0)
-                {
-                    tutorials[0].SetActive(true);
-                    currentIndex = 1;  
-                }
-            }
-            else if (currentIndex < tutorials.Length)
-            {
-                tutorials[currentIndex - 1].SetActive(false);
-                tutorials[currentIndex].SetActive(true);
-                currentIndex++;
-            }
-            else
-            {
-                tutorials[currentIndex - 1].SetActive(false);
-            }
-        }
-    }
+//                if (tutorials.Length > 0)
+//                {
+//                    tutorials[0].SetActive(true);
+//                    currentIndex = 1;  
+//                }
+//            }
+//            else if (currentIndex < tutorials.Length)
+//            {
+//                tutorials[currentIndex - 1].SetActive(false);
+//                tutorials[currentIndex].SetActive(true);
+//                currentIndex++;
+//            }
+//            else
+//            {
+//                tutorials[currentIndex - 1].SetActive(false);
+//            }
+//        }
+//    }
 
-    private void Awake()
-    {
-        UIElement.SetActive(true);
+//    private void Awake()
+//    {
+//        UIElement.SetActive(true);
 
-        foreach (GameObject tutorial in tutorials)
-        {
-            tutorial.SetActive(false);
-        }
+//        foreach (GameObject tutorial in tutorials)
+//        {
+//            tutorial.SetActive(false);
+//        }
 
-        if (!_hasInitialized && tutorials.Length > 0)
-        {
-            tutorials[0].SetActive(true);
-            _hasInitialized = true;  
-            currentIndex = 1; 
-        }
-    }
-}
+//        if (!_hasInitialized && tutorials.Length > 0)
+//        {
+//            tutorials[0].SetActive(true);
+//            _hasInitialized = true;  
+//            currentIndex = 1; 
+//        }
+//    }
+//}
