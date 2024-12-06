ff38b7ab45e30f606854456e4a428bf5ade447b4
Liza Platonov <eplatonov@middlebury.edu>
2024-12-06 21:37:48 +0100

    cleaned a little

--- Assets/Scripts/UIScripts/PieceMenu/SelectPiece.cs
+++ Assets/Scripts/UIScripts/PieceMenu/SelectPiece.cs
@@ -2,16 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//public enum PieceType
-//{
-//    Rook,
-//    Bishop,
-//    Knight,
-//    Pawn,
-//    King
-//}
-
-
 public class SelectPiece : MonoBehaviour
 {
     [Header("Chess Board Reference")]
@@ -67,10 +57,6 @@ public class SelectPiece : MonoBehaviour
         }
 
     }
-
-   
-
-   
 
     public GameObject InstantiatePieceOnBoard(Vector3 position, GameObject piecePrefab)
     {
