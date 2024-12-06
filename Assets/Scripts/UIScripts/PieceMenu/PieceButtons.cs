874441027e31a17fec3a2ae00dba22d21f11d946
Liza Platonov <eplatonov@middlebury.edu>
2024-12-06 15:45:08 +0100

    Changed PieceBUtton class with jorge

--- Assets/Scripts/UIScripts/PieceMenu/PieceButtons.cs
+++ Assets/Scripts/UIScripts/PieceMenu/PieceButtons.cs
@@ -23,45 +23,44 @@ public class PieceButtons : MonoBehaviour
 
     [Header("Other Scripts")]
     [SerializeField] chessPuzzleSpawner puzzleSpawner;
+    [SerializeField] PieceButtonMonitor pieceButtonMonitor;
 
     private List<GameObject> pieceMenuButtonList;
 
     public void CreatePieceMenu(List<GameObject> pieceMenu)
     {
+        GameObject button = null;
         pieceMenuButtonList = new List<GameObject>();
 
         foreach (GameObject piece in pieceMenu)
         {
-            GameObject button = null;
-
-            if (piece.CompareTag("rook"))
+            string pieceTag = piece.gameObject.tag;
+            switch (pieceTag)
             {
-                button = Instantiate(rookButtonPrefab, pieceMenuPanel.transform);
-                button.GetComponent<Button>().onClick.AddListener(() => GetComponent<SelectPiece>().PieceSelected(rookPrefab, button.GetComponent<Button>()));
-            }
-            else if (piece.CompareTag("king"))
-            {
-                button = Instantiate(kingButtonPrefab, pieceMenuPanel.transform);
-                button.GetComponent<Button>().onClick.AddListener(() => GetComponent<SelectPiece>().PieceSelected(kingPrefab, button.GetComponent<Button>()));
-            }
-            else if (piece.CompareTag("bishop"))
-            {
-                button = Instantiate(bishopButtonPrefab, pieceMenuPanel.transform);
-                button.GetComponent<Button>().onClick.AddListener(() => GetComponent<SelectPiece>().PieceSelected(bishopPrefab, button.GetComponent<Button>()));
-            }
-            else if (piece.CompareTag("pawn"))
-            {
-                button = Instantiate(pawnButtonPrefab, pieceMenuPanel.transform);
-                button.GetComponent<Button>().onClick.AddListener(() => GetComponent<SelectPiece>().PieceSelected(pawnPrefab, button.GetComponent<Button>()));
-            }
-            else if (piece.CompareTag("knight"))
-            {
-                button = Instantiate(knightButtonPrefab, pieceMenuPanel.transform);
-                button.GetComponent<Button>().onClick.AddListener(() => GetComponent<SelectPiece>().PieceSelected(knightPrefab, button.GetComponent<Button>()));
+                case "rook":
+                    button = SpawnPiece(rookPrefab, rookButtonPrefab);
+                    break;
+                case "king":
+                    button = SpawnPiece(kingPrefab, kingButtonPrefab);
+                    break;
+                case "bishop":
+                    button = SpawnPiece(bishopPrefab, bishopButtonPrefab);
+                    break;
+                case "pawn":
+                    button = SpawnPiece(pawnPrefab, pawnButtonPrefab);
+                    break;
+                case "knight":
+                    button = SpawnPiece(knightPrefab, knightButtonPrefab);
+                    break;
+                default:
+                    Debug.LogWarning("No valid case was given");
+                    return;
             }
 
             pieceMenuButtonList.Add(button);
         }
+
+        pieceButtonMonitor.StartMonitoringPieceButtons(pieceMenuButtonList);
     }
 
     public void ResetPieceMenu()
@@ -72,5 +71,19 @@ public class PieceButtons : MonoBehaviour
         }
 
         GetComponent<SelectPiece>().ResetBoard();
+    }
+
+    private GameObject SpawnPiece(GameObject piecePrefab, GameObject pieceButton)
+    {
+        GameObject button = Instantiate(pieceButton, pieceMenuPanel.transform);
+        SelectPiece selectPieceComponent = GetComponent<SelectPiece>();
+        Button buttonComponent = button.GetComponent<Button>();
+        buttonComponent.onClick.AddListener(() => selectPieceComponent.PieceSelected(piecePrefab, buttonComponent));
+        return button;
+    }
+
+    public List<GameObject> getButtonMenu()
+    {
+        return pieceMenuButtonList;
     }
 }
